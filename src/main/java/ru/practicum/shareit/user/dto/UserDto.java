7c884226b4ea0d87b157f7d71a3f70c6e9bace92
vlasabo <vladimirsabo@yandex.ru>
2022-12-19 17:39:19 +0300

    feat: добавить тестирование UserController

--- src/main/java/ru/practicum/shareit/user/dto/UserDto.java
+++ src/main/java/ru/practicum/shareit/user/dto/UserDto.java
@@ -2,10 +2,12 @@ package ru.practicum.shareit.user.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import ru.practicum.shareit.user.model.User;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 public class UserDto extends User {
